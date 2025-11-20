04c516354a23e7fc7e3c51af6c33271ed5829b65
Yoel Ayan <yoeldategeek@gmail.com>
2025-11-20 20:11:54 +0000

    Change default serve permissions from IsAdminUser to AllowAny in base settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -319,7 +319,7 @@ SPECTACULAR_SETTINGS = {
         "métricas."
     ),
     "VERSION": "1.0.0",
-    "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
+    "SERVE_PERMISSIONS": ["rest_framework.permissions.AllowAny"],  # Changed from IsAdminUser
     "SERVE_AUTHENTICATION": None,
     "SCHEMA_PATH_PREFIX": "/api/",
     "SCHEMA_PATH": "/api/schema/",
