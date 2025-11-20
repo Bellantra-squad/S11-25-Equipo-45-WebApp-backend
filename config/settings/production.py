1c9207becb01e516dec087195223d0b5e3538702
Yoel Ayan <yoeldategeek@gmail.com>
2025-11-20 20:03:52 +0000

    Update staticfiles storage backend in production settings for improved performance

--- config/settings/production.py
+++ config/settings/production.py
@@ -70,7 +70,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
